bf5151138b13dcd0e3b173aca7ab9047a8b57d07
Kevin Haro <artcom_3@hotmail.com>
2021-09-18 19:27:11 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const links = [
+    {
+        label: "Week1 notes",
+        url: "week01/index.html"
+    }
+]
+
+function printLinksHtml() {
+    const ol = document.querySelector('#links');
+    
+    links.forEach(link => {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+
+        let aLink = document.createTextNode(link.label);
+        a.appendChild(aLink);
+        a.title = link.label;
+        a.href = link.url
+
+        li.appendChild(a);
+        ol.appendChild(li);
+    });
+}
+
+printLinksHtml();
